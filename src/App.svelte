6c36b73cac6c2b8d044ee44cd30c83b530954607
Luke Harby <lukeharby@Lukes-MacBook-Pro.local>
2023-11-21 21:45:23 +0000

    Update tabs

--- src/App.svelte
+++ src/App.svelte
@@ -4,30 +4,45 @@
 
     let nav = [
         {
+            id: 0,
             name: "index",
             path: "index.html",
-            label: "index.html"
+            label: "index.html",
+            className: "active",
+            content: `<p class="pink">Hello, I'm Luke. I'm a web developer and expert duck wrangler<p><p class="yellow">If you want to contact me</p>`
         },
         {
+            id: 1,
             name: "about",
             path: "/about-us",
-            label: "about-us.html"
+            label: "about-us.html",
+            className: "",
+            content: `<p class="blue">About</p>`
         },
         {
+            id: 2,
             name: "photos",
             path: "/photos",
-            label: "photos.html"
+            label: "photos.html",
+            className: "",
+            content: `<p class="green">Photos</p>`
         },
         {
+            id: 3,
             name: "conact",
             path: "/contact",
-            label: "contact.html"
+            label: "contact.html",
+            className: "",
+            content: `<p class="purple">Contact</p>`
         }
     ];
 
     const handleSideNav = (event) => {
         event.preventDefault();
         console.log(event.target);
+        const navItems = document.querySelectorAll('.top-nav a');
+        navItems.forEach(item => item.classList.remove('active'));
+        event.target.classList.add('active');
     }
 </script>
 
@@ -36,14 +51,14 @@
         <li>App
             <ul>
                 {#each nav as item}
-                    <SideNav path={item.path} label={item.label} on:click={handleSideNav}/>        
+                    <SideNav path={item.path} label={item.label} on:click={handleSideNav} /> 
                 {/each}
             </ul>
         </li>
     </ul>
     <ul class="top-nav">
         {#each nav as item}
-            <TopNav path={item.path} label={item.label} on:click={handleSideNav} />
+            <TopNav path={item.path} className={item.className} label={item.label} on:click={handleSideNav} content={item.content}/>
         {/each}
     </ul>
 </main>
@@ -85,12 +100,18 @@
         bottom: 15%;
         left: 10%;
         border: 1px solid $colour__white;
+        border-top: none;
         border-radius: 8px;
         overflow-y: scroll;
     }
 
     .top-nav {
         display: flex;
+        position: relative;
+        width: 100%;
+        height: 30px;
+        z-index: -1;
+        background-color: $colour__grey-pale;
     }
 
     .side-nav {
@@ -99,11 +120,12 @@
         height: 100%;
         width: 25%;
         font-family: sans-serif;
+        padding: 1rem 0 0 1rem;
+        min-height: 100%;
 
         ul {
             margin: 0 0 0 1.5rem;
             padding: unset;
         }
     }
-
 </style>
--- src/ui/TopNav.svelte
+++ src/ui/TopNav.svelte
@@ -1,9 +1,15 @@
 <script>
     export let path;
     export let label;
+    export let className = '';
+    export let content;
 </script>
 
-<li><a href={path} on:click>{label}<span>x</span></a></li>
+<li><a href={path} class={className} on:click>{label}<span>x</span></a>
+    <ul>
+        <li>{@html content}</li>
+    </ul>
+</li>
 
 <style lang="scss">
     @import '../scss/variables.scss';
@@ -17,10 +23,11 @@
     }
 
     a {
+        display: flex;
         position: relative;
         color: $colour__white;
         text-decoration: none;
-        padding: 0.25rem 1.5rem 0 2.5rem;
+        padding: 0 1.5rem 0 2.5rem;
 
         &:before {
             content: "";
@@ -30,17 +37,31 @@
             border-right: 20px solid transparent;
             height: 0;
             width: 100%;
+            left: 0;
             position: absolute;
             z-index: -1;
         }
 
-        &.active:before {
-            border-bottom-color: $colour__grey-charcoal;
+        &.active {
+            &:before {
+                border-bottom-color: $colour__grey-dark;
+            }
+
+            ~ ul {
+                display: block;
+                left: 0;
+            }
         }
 
         span {
             position: absolute;
             right: -1rem;
         }
+    }
+
+    ul {
+        display: none;
+        position: absolute;
+        padding: 4rem;
     }
 </style>
