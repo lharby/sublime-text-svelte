36d2827d93e422e92c8a0e46cba7de3c58bd77f6
Luke Harby <lukeharby@Lukes-MacBook-Pro.local>
2023-11-21 19:34:32 +0000

    Update tabs

--- src/ui/TopNav.svelte
+++ src/ui/TopNav.svelte
@@ -9,7 +9,7 @@
     @import '../scss/variables.scss';
 
     li {
-        margin-right: 2rem;
+        margin-right: 2.6rem;
 
         &:last-child {
             margin-right: 0;
@@ -17,7 +17,30 @@
     }
 
     a {
+        position: relative;
         color: $colour__white;
         text-decoration: none;
+        padding: 0.25rem 1.5rem 0 2.5rem;
+
+        &:before {
+            content: "";
+            display: flex;
+            border-bottom: 30px solid $colour__grey-mid;
+            border-left: 20px solid transparent;
+            border-right: 20px solid transparent;
+            height: 0;
+            width: 100%;
+            position: absolute;
+            z-index: -1;
+        }
+
+        &.active:before {
+            border-bottom-color: $colour__grey-charcoal;
+        }
+
+        span {
+            position: absolute;
+            right: -1rem;
+        }
     }
 </style>
